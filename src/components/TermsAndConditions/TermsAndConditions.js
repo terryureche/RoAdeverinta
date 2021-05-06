b197a9e2de56727ab0eb7a9fce30b74dfd0b5a78
Vlad Drinceanu <vlad.drinceanu@digitalkrikits.co.uk>
2021-05-06 21:31:38 +0300

    move terms and conditions to a dedicated modal

--- src/components/TermsAndConditions/TermsAndConditions.js
+++ src/components/TermsAndConditions/TermsAndConditions.js
@@ -1,6 +1,59 @@
-import React from "react";
+import React, {Fragment} from "react";
+
+import {IonButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, useIonModal} from "@ionic/react";
+
+const TermsNConditionsBody = ({onDismiss}) => {
+    return (
+        <Fragment>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Termeni si Conditii</IonTitle>
+                    <IonButtons slot="end">
+                        <IonButton onClick={() => onDismiss()}>OK</IonButton>
+                    </IonButtons>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <div className="w-full h-full p-6">
+                    <ul className="list-disc list-inside">
+                        <li>
+                            Dezvoltatorii aplicatiei curente nu își asumă nicio răspundere referitoare la furnizarea
+                            eronată
+                            a
+                            informațiilor solicitate pe baza cărora este generat documentul solicitat.
+                        </li>
+                        <li>
+                            Datele introduse in aplicatie sunt folosite strict pentru generarea documentului solicitat
+                            si
+                            siguranta
+                            lor este oferita de sistemul de operare al dispozitivului folosit de utilizatorul
+                            aplicatiei.
+                        </li>
+                        <li>
+                            Dezvoltatorii aplicatiei nu isi asuma nicio raspundere referitoare la validitatea
+                            documentului
+                            generat
+                            la data solicitarii acestuia.
+                            Utilizatorul trebuie sa se asigure ca documentul generat este in conformitate cu legislatia
+                            in
+                            vigoare
+                            la momentul folosirii aplicatiei.
+                        </li>
+                    </ul>
+                </div>
+            </IonContent>
+        </Fragment>
+    );
+}
 
 const TermsAndConditions = () => {
+    const handleDismiss = () => {
+        dismiss();
+    }
+    const [present, dismiss] = useIonModal(TermsNConditionsBody, {
+        onDismiss: handleDismiss
+    });
+
     return (
         <div className="w-105-screen h-full flex flex-col overflow-y-auto pb-3">
             <img
@@ -17,20 +70,10 @@ const TermsAndConditions = () => {
                     </span>
                 </h2>
                 <div className="mt-4">
-                    Prin continuarea folosirii aplicatiei inseamna ca sunteti de acord cu urmatorii Termeni si Conditii:
-                    <ul className="list-disc mt-2 list-inside">
-                        <li>
-                            Dezvoltatorii aplicatiei curente nu își asumă nicio răspundere referitoare la furnizarea eronată a informațiilor solicitate pe baza cărora este generat documentul solicitat.
-                        </li>
-                        <li>
-                            Datele introduse in aplicatie sunt folosite strict pentru generarea documentului solicitat si siguranta lor este oferita de sistemul de operare al dispozitivului folosit de utilizatorul aplicatiei.
-                        </li>
-                        <li>
-                            Dezvoltatorii aplicatiei nu isi asuma nicio raspundere referitoare la validitatea documentului generat la data solicitarii acestuia.
-                            Utilizatorul trebuie sa se asigure ca documentul generat este in conformitate cu legislatia in vigoare la momentul folosirii aplicatiei.
-                        </li>
-                    </ul>
-
+                    Prin continuarea folosirii aplicatiei inseamna ca esti de acord cu Termenii si Conditiile.
+                    <button className="underline-warning" onClick={() => present()}>
+                        Vezi Termeni si Conditii aici
+                    </button>
                 </div>
             </div>
         </div>
